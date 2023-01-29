5c8a95b8a70a1fe717615c4f8ad13bac84adb064
Nick Forleo <nickm140mc@gmail.com>
2023-01-29 13:07:06 -0500

    basic landing page

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,3 +1,5 @@
+import { ChangeEventHandler, useCallback, useState } from "react";
+import { Dropdown, Form } from "react-bootstrap";
 import styled from "styled-components";
 import { AppLogo } from "./Logo";
 
@@ -5,8 +7,54 @@ const HomeScreenStyle = styled.div`
     width: 100%;
     height: 100%;
     background-image: linear-gradient(black, gray);
+
+
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    
+  
 `;
 
 export function HomeScreen () {
-  return  <HomeScreenStyle><AppLogo /></HomeScreenStyle>
+  const [text, setText] = useState<string>("");
+
+  const onChange = useCallback<ChangeEventHandler<HTMLInputElement>>((e) => {
+    setText(e.target.value);
+  }, []);
+
+  return  (<HomeScreenStyle>
+        <AppLogo />
+        <p>Begin searching for a song or arist</p>
+        <Dropdown>
+          <Dropdown.Toggle 
+            as={Form.Control}
+            onChange={onChange}
+            value={text}
+          />
+          {text.length < 3 ? null : (<Dropdown.Menu>
+            <Dropdown.Header>Songs</Dropdown.Header>
+            <Dropdown.Item>
+              Song 1
+            </Dropdown.Item>
+            <Dropdown.Item>
+              Song 2
+            </Dropdown.Item>
+            <Dropdown.Item>
+              Song 3
+            </Dropdown.Item>
+            <Dropdown.Divider />
+            <Dropdown.Header>Artists</Dropdown.Header>
+            <Dropdown.Item>
+              Artist 1
+            </Dropdown.Item>
+            <Dropdown.Item>
+              Artist 2
+            </Dropdown.Item>
+            <Dropdown.Item>
+              Artist 3
+            </Dropdown.Item>
+          </Dropdown.Menu>) }
+        </Dropdown>
+    </HomeScreenStyle>)
 }
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,3 +1,3 @@
 export function AppLogo () {
-    return <img src={process.env.PUBLIC_URL + '/img/site_logo.png'} height="256px" alt="Application Logo" />
+    return <img src={process.env.PUBLIC_URL + '/img/site_logo.png'} height="256px" width="256px" alt="Application Logo" />
 }
